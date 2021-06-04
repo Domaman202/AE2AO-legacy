9b004d978a6845c853a5d71aa3a7febfef406e9b
Domaman202 <vip.domaman@mail.ru>
2021-06-04 18:10:02 +0700

    .

--- src/main/java/ru/DmN/AE2AO/Example.java
+++ src/main/java/ru/DmN/AE2AO/Example.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Example.MOD_ID)
 public class Example {
-    public static final String MOD_ID = "example";
+    public static final String MOD_ID = "ae2ao";
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
